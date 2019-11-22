4c5222d47b0fd79ff01664ac1b3d8040181e9307
Graeme Hosford <graemehosford@gmail.com>
2019-11-22 16:38:50 +0000

    Missed these in last commit

---
+++ src/main/java/graeme/hosford/rob/morgan/assignment02restconsumer/controllers/BidForm.java
@@ -0,0 +1,13 @@
+package graeme.hosford.rob.morgan.assignment02restconsumer.controllers;
+
+public class BidForm {
+    private long userId;
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+}
---
+++ src/main/java/graeme/hosford/rob/morgan/assignment02restconsumer/entities/UserBid.java
@@ -0,0 +1,29 @@
+package graeme.hosford.rob.morgan.assignment02restconsumer.entities;
+
+public class UserBid {
+    private long bidId;
+    private double bidAmount;
+
+    public UserBid() {}
+
+    public UserBid(long bidId, double bidAmount) {
+        this.bidId = bidId;
+        this.bidAmount = bidAmount;
+    }
+
+    public double getBidAmount() {
+        return bidAmount;
+    }
+
+    public void setBidAmount(double bidAmount) {
+        this.bidAmount = bidAmount;
+    }
+
+    public long getBidId() {
+        return bidId;
+    }
+
+    public void setBidId(long bidId) {
+        this.bidId = bidId;
+    }
+}
